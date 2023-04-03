5ed4aab4bfb91af51394c8d9452902db62e96c66
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2023-04-03 13:29:52 +0300

    Use explicit make targets for non-Linux AMD64
    Based on https://github.com/containers/skopeo/blob/v1.6.1/Makefile#L158-L167
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,12 @@ module_path="${GOPATH:-"$( go env GOPATH )"}"/src/github.com/containers/skopeo
 mkdir -p "$( dirname "${module_path}" )"
 mv ./src "${module_path}"
 
-export GO111MODULE=auto
+export GO111MODULE=on
+
+disable_cgo=0
+if ! [[ ${target_platform} =~ linux.* ]] ; then
+    disable_cgo=1
+fi
 
 export GOARCH="amd64"
 export GOOS=$(uname -s | tr '[:upper:]' '[:lower:]')
@@ -15,25 +20,34 @@ ARCH=$(uname -m)
 case $ARCH in
     amd64 | x86_64)
         export GOARCH="amd64"
+        make -C "${module_path}" install \
+            DISABLE_CGO="${disable_cgo}" \
+            CONTAINERSCONFDIR="${PREFIX}/share/containers" \
+            LOOKASIDEDIR="${PREFIX}/etc/containers/sigstore" \
+            GIT_COMMIT=
         ;;
     arm64 | aarch64)
         export GOARCH="arm64"
+        pushd "${module_path}"
+        make bin/skopeo.${GOOS}.${GOARCH} \
+            DISABLE_CGO="${disable_cgo}" \
+            CONTAINERSCONFDIR="${PREFIX}/share/containers" \
+            LOOKASIDEDIR="${PREFIX}/etc/containers/sigstore" \
+            GIT_COMMIT=
+        install -d -m 755 ${DESTDIR}${BINDIR}
+        install -m 755 bin/skopeo.${GOOS}.${GOARCH} ${DESTDIR}${BINDIR}/skopeo
+        make install-docs install-completions \
+            DISABLE_CGO="${disable_cgo}" \
+            CONTAINERSCONFDIR="${PREFIX}/share/containers" \
+            LOOKASIDEDIR="${PREFIX}/etc/containers/sigstore" \
+            GIT_COMMIT=
+        popd
         ;;
     *)
         echo "Unsupported architecture: $ARCH"
         exit 1
         ;;
 esac
-
-disable_cgo=0
-if ! [[ ${target_platform} =~ linux.* ]] ; then
-    disable_cgo=1
-fi
-make -C "${module_path}" install \
-  DISABLE_CGO="${disable_cgo}" \
-  CONTAINERSCONFDIR="${PREFIX}/share/containers" \
-  LOOKASIDEDIR="${PREFIX}/etc/containers/sigstore" \
-  GIT_COMMIT=
 
 export THIRD_PARTY_LICENSES="${PWD}/3rd_party_licenses/"
 pushd "${module_path}"
