1d54df6905595a4845dcdcb18a973b46751f5e23
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2023-04-03 14:27:11 +0300

    Do not use DESTDIR and BINDIR for linux-aarch64
    Those are defined only when executed via "make" but 'install/binary'
    target is not prepared for cross compilation
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,48 +7,38 @@ module_path="${GOPATH:-"$( go env GOPATH )"}"/src/github.com/containers/skopeo
 mkdir -p "$( dirname "${module_path}" )"
 mv ./src "${module_path}"
 
-export GO111MODULE=on
-
 disable_cgo=0
 if ! [[ ${target_platform} =~ linux.* ]] ; then
     disable_cgo=1
 fi
 
-declare -a os_arch=($(echo $target_platform | tr "-" "\n"))
+export GOOS=$(go env GOOS)
+export GOARCH=$(go env GOARCH)
 
-export GOOS="${os_arch[0]}"
-export GOARCH="${os_arch[1]}"
-case $GOARCH in
-    amd64 | x86_64)
-        export GOARCH="amd64"
-        make -C "${module_path}" install \
+if [[ ${GOARCH} == "arm64" ]] ; then
+    pushd "${module_path}"
+    
+    make bin/skopeo.${GOOS}.${GOARCH} \
+        DISABLE_CGO="${disable_cgo}" \
+        CONTAINERSCONFDIR="${PREFIX}/share/containers" \
+        LOOKASIDEDIR="${PREFIX}/etc/containers/sigstore" \
+        GIT_COMMIT=
+    install -d -m 755 "${PREFIX}/bin"
+    install -m 755 bin/skopeo.${GOOS}.${GOARCH} "${PREFIX}/bin/skopeo"
+    make install-docs \
+        DISABLE_CGO="${disable_cgo}" \
+        CONTAINERSCONFDIR="${PREFIX}/share/containers" \
+        LOOKASIDEDIR="${PREFIX}/etc/containers/sigstore" \
+        GIT_COMMIT=
+    
+    popd
+else
+    make -C "${module_path}" install \
             DISABLE_CGO="${disable_cgo}" \
             CONTAINERSCONFDIR="${PREFIX}/share/containers" \
             LOOKASIDEDIR="${PREFIX}/etc/containers/sigstore" \
             GIT_COMMIT=
-        ;;
-    arm64 | aarch64)
-        export GOARCH="arm64"
-        pushd "${module_path}"
-        make bin/skopeo.${GOOS}.${GOARCH} \
-            DISABLE_CGO="${disable_cgo}" \
-            CONTAINERSCONFDIR="${PREFIX}/share/containers" \
-            LOOKASIDEDIR="${PREFIX}/etc/containers/sigstore" \
-            GIT_COMMIT=
-        install -d -m 755 ${DESTDIR}${BINDIR}
-        install -m 755 bin/skopeo.${GOOS}.${GOARCH} ${DESTDIR}${BINDIR}/skopeo
-        make install-docs install-completions \
-            DISABLE_CGO="${disable_cgo}" \
-            CONTAINERSCONFDIR="${PREFIX}/share/containers" \
-            LOOKASIDEDIR="${PREFIX}/etc/containers/sigstore" \
-            GIT_COMMIT=
-        popd
-        ;;
-    *)
-        echo "Unsupported architecture: $ARCH"
-        exit 1
-        ;;
-esac
+fi
 
 export THIRD_PARTY_LICENSES="${PWD}/3rd_party_licenses/"
 pushd "${module_path}"
