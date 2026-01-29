52131adb9a7bc4157b118388bcb7dc8786e9fe84
Jan Jagusch <jan.jagusch@gmail.com>
2026-01-29 10:48:59 +0100

    Always disable cgo

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,13 +6,8 @@ cd src
 
 go-licenses save ./cmd/skopeo --save_path="../library_licenses"
 
-disable_cgo=0
-if ! [[ ${target_platform} =~ linux.* ]] ; then
-    disable_cgo=1
-fi
-
 make -C . install \
-        DISABLE_CGO="${disable_cgo}" \
+        DISABLE_CGO="1" \
         CONTAINERSCONFDIR="${PREFIX}/share/containers" \
         LOOKASIDEDIR="${PREFIX}/etc/containers/sigstore" \
         GIT_COMMIT=
