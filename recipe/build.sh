2ce93429c9f0a49aaa81ff7c51a9b84a62196a4e
Jan Jagusch <jan.jagusch@gmail.com>
2026-01-29 11:09:40 +0100

    Progress

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ cd src
 go-licenses save ./cmd/skopeo --save_path="../library_licenses"
 
 make -C . install \
-        DISABLE_CGO="1" \
-        CONTAINERSCONFDIR="${PREFIX}/share/containers" \
-        LOOKASIDEDIR="${PREFIX}/etc/containers/sigstore" \
-        GIT_COMMIT=
+    DISABLE_CGO="1" \
+    CONTAINERSCONFDIR="${PREFIX}/share/containers" \
+    LOOKASIDEDIR="${PREFIX}/etc/containers/sigstore" \
+    GIT_COMMIT=
